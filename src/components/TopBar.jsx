3ca964ff8433d4e40117768d74c0add39130d8a0
kpomm <katherine.l.pommerening.25@dartmouth.edu>
2023-09-10 01:10:31 -0400

    Styling updates

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import logo from './pixelLogo.png';
 
 function TopBar() {
   return (
     <div className="top-bar">
       <div className="flex-row">
         <h1>kpshoot5</h1>
-        <img id="logo" src="./pixelLogo.png" alt="" />
+        <img id="logo" src={logo} alt="" />
       </div>
       <h2>LVL1: scared (B&W)</h2>
     </div>
