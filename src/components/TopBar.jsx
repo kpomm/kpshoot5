175a13b77a1fa5a5f97e9dc69fd9cb9e5cdaf377
kpomm <katherine.l.pommerening.25@dartmouth.edu>
2023-09-10 00:43:29 -0400

    Initial commit

---
+++ src/components/TopBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function TopBar() {
+  return (
+    <div className="top-bar">
+      <h1>kpshoot5</h1>
+      <h2>LVL1: scared (B&W)</h2>
+    </div>
+  );
+}
+
+export default TopBar;
---
+++ src/components/app.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import TopBar from './TopBar';
+
+function App(props) {
+  return (
+    <div>
+      <TopBar />
+    </div>
+  );
+}
+
+export default App;
