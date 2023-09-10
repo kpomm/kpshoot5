c8030b660a8f805a92329c009c997c965ae0e179
kpomm <katherine.l.pommerening.25@dartmouth.edu>
2023-09-10 01:05:26 -0400

    Added logo

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 function TopBar() {
   return (
     <div className="top-bar">
-      <h1>kpshoot5</h1>
+      <div className="flex-row">
+        <h1>kpshoot5</h1>
+        <img id="logo" src="./pixelLogo.png" alt="" />
+      </div>
       <h2>LVL1: scared (B&W)</h2>
     </div>
   );
