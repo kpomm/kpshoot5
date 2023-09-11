89c2af579dc68b53646f51ff9bf5eea38d78c560
kpomm <katherine.l.pommerening.25@dartmouth.edu>
2023-09-11 01:38:36 -0400

    Still troubleshooting photo gallery

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import TopBar from './TopBar';
+import PhotoGallery from './PhotoGallery';
 
 function App(props) {
   return (
     <div>
       <TopBar />
+      <PhotoGallery />
     </div>
   );
 }
